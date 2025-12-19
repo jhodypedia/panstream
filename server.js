f277cbd67ac57818ade6836e8e54b6c1c64d1a8f
jhodypedia <87615107+jhodypedia@users.noreply.github.com>
2025-12-20 02:36:55 +0700

    Initialize Express server with routing and middleware

---
+++ server.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import path from "path";
+import { fileURLToPath } from "url";
+
+import pageRoutes from "./routes/page.js";
+import apiRoutes from "./routes/api.js";
+
+dotenv.config();
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+
+app.use("/", pageRoutes);
+app.use("/api", apiRoutes);
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`🔥 http://localhost:${process.env.PORT || 3000}`);
+});
